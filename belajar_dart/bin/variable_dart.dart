e62c9fb34094a2a119d0430a412182a3e001444d
OrizaAria <orizadioaria1705@gmail.com>
2024-06-19 18:27:18 +0700

    Dart_Materi Variable

--- belajar_dart/bin/variable_dart.dart
+++ belajar_dart/bin/variable_dart.dart
@@ -5,9 +5,36 @@ void main() {
   int usia = 26;
   var tinggi = 175;
   var bulan = "Mei";
+
+  final tgl = 17;
+
   print(nama);
   print(usia);
   print(tinggi);
+  print(tgl);
   print(bulan);
-  print(nama);
+
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 34; // nilai pada variabel final masih bisa diubah
+  // array2[0] = 9; // nilai pada variabel const tidak bisa diubah
+
+  print(array1);
+  print(array2);
+
+  print('');
+  var value = getValue();
+  print('Variabel 1 sudah dibuat');
+  print(value);
+  print('');
+
+  late var value2 = getValue();
+  print('Variabel 2 sudah dibuat');
+  print(value2);
+}
+
+String getValue() {
+  print('getValue dipanggil');
+  return 'Sativa Maylafaisha';
 }
