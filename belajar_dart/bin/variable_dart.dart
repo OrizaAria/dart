7043b3297b96b7888db8765a061f87af39f9a90f
OrizaAria <orizadioaria1705@gmail.com>
2024-06-19 17:55:39 +0700

    Dart_Variable

---
+++ belajar_dart/bin/variable_dart.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nama;
+  nama = "Oriza Dio Aria";
+
+  int usia = 26;
+  var tinggi = 175;
+  var bulan = "Mei";
+  print(nama);
+  print(usia);
+  print(tinggi);
+  print(bulan);
+  print(nama);
+}
